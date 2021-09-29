46d3e652878a5f3cde1ce5e7cd2361d60769587e
ellenkirsh <ellenek2@illinois.edu>
2021-09-29 18:15:15 -0500

    Update main.cpp

--- ellenek2_hw4_4/main.cpp
+++ ellenek2_hw4_4/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int *MergeArrays(int *a, int *b, int sizea, int sizeb){
+    int size = sizea + sizeb;
+    int *newarray;
+    newarray = new int[size];
+    for(int i=0; i<sizea; i++){
+        newarray[i] = a[i];
+    }
+    for(int i=0; i<sizeb; i++){
+        newarray[sizea+i] = b[i];
+    }
+    for(int i=0; i<size; i++){
+        for(int j=i+1; j<size;j++){
+            if(newarray[i]>newarray[j]){
+                int temp = newarray[i];
+                newarray[i]=newarray[j];
+                newarray[j]=temp;
+            }
+        }
+    }
+    cout << "The new array is: ";
+    for(int i=0; i<size; i++){
+        cout << newarray[i] << " ";
+    }
+    return 0;
+}
+
+int main() {
+    int a[] = {4, 3, 7, 8, 6};
+    int b[] = {2, 5, 3};
+    int sa = 5;
+    int sb = 3;
+    MergeArrays(a, b, sa, sb);
+    cout << endl;
     return 0;
 }
